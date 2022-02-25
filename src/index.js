449d088139ebead7e49fe691cc427885059ec90c
Anastasiya <Anastasiya@sergeys-mbp.kpn>
2022-02-25 12:22:26 +0100

    feat: add functions codeToArray, toMorse, decode for decode data

--- src/index.js
+++ src/index.js
@@ -37,8 +37,61 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+const decode = (code) => {
+    let array = codeToArray(code);
+    let arrayMorse = toMorse(array);
+    
+    let newStr = '';
+    arrayMorse.forEach( element => {
+        if(element === '*') newStr += ' ';
+        else {
+            newStr += MORSE_TABLE[element];
+        }
+        
+    })
+    
+   return newStr;
+}
+
+const codeToArray = (code) => {
+    let array = [];
+    let i = 0;
+    let index = 0;
+    while( i !== code.length) {
+        array[index] = code.slice(i, i + 10);
+        index++;
+        i += 10;
+    }
+    return array;
+}
+
+const toMorse = (array) => {
+    let arrayMorse = new Array(array.length);
+    arrayMorse.fill('');
+    let temp = '';
+    let j = 0;
+    let i;
+    array.forEach( item => {
+        if (item === '**********') {
+            arrayMorse[j] += '*';
+            j++;
+        } else {
+            i = 0;
+            while( i !== item.length) {
+                temp = item.slice(i, i + 2);
+        
+                if(temp === '10') {
+                    arrayMorse [j] += '.';
+                }
+                if(temp === '11') {
+                    arrayMorse [j] += '-';
+                }
+                i += 2;
+            }
+            j++; 
+        }
+    });
+    return arrayMorse;
 }
 
 module.exports = {
